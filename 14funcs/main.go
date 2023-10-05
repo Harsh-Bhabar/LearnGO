52065fdca8e6d37bf60d12eaacac45c8a7fe4751
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2023-10-05 11:49:01 +0530

    funcs

--- 14funcs/main.go
+++ 14funcs/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("Functions")
+	hello()
+
+	sum := add(2, 5)
+	fmt.Printf("sum is %v\n", sum)
+
+	sum2, sum2Str := proadd(1, 2, 2, 4, 4, 5, 6, 2, 5, 5, 5)
+	fmt.Printf("sum is %v and returned str is %v\n", sum2, sum2Str)
+
+}
+
+func proadd(values ...int) (int, string) {
+	total := 0
+
+	for _, val := range values {
+		total += val
+	}
+
+	return total, "PROADD"
+}
+
+func add(val1 int, val2 int) int {
+	return val1 + val2
+}
+
+func hello() {
+	fmt.Println("hello")
 }
